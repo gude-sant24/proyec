c17fd049b861ba95cc16e8f6a13bc68f31346a8b
santiago <gudelomosquerasan4334@gmail.com>
2026-09-23 17:27:39 -0500

    actualize un ejercicio

--- tabla.py
+++ tabla.py
@@ -1,4 +1,4 @@
-numero_multiplicacion = int(input("ingrese el numero a multiplicar"))
-tabla = int(input("ingrese las veces que se va a multiplicar"))
+numero_multiplicacion = int(input("ingrese el numero a multiplicar "))
+tabla = int(input("ingrese las veces que se va a multiplicar "))
 for i in range(1, tabla + 1):
-    print(numero_multiplicacion * i)
+    print(str(numero_multiplicacion) + "x" + str(i) + "=" + str(numero_multiplicacion * i))
